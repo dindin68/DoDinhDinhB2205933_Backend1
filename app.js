d5c50cd43ee0d0defbc435ee45001d4b51955e68
DinhDinh <dinwork04@gmail.com>
2025-10-22 10:14:34 +0700

    Dinh ghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+const contractRoutes = require('./app/routes/contract.route');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/contracts', contractRoutes);
 
 app.get('/', (req, res) => {
     res.json({ message: 
---
+++ app/controllers/contract.controller.js
@@ -0,0 +1,31 @@
+exports.create = (req, res) => {
+    // Logic to create a contract
+    res.send({ message: 'create handler' });
+}
+
+exports.findAll = (req, res) => {
+    res.send({ message: 'findAll handler' });
+}
+
+exports.findOne = (req, res) => {
+    const id = req.params.id;
+    res.send({ message: 'findOne handler' });
+}
+
+exports.update = (req, res) => {
+    const id = req.params.id;
+    res.send({ message: 'update handler' });
+}
+
+exports.delete = (req, res) => {
+    const id = req.params.id;
+    res.send({ message: 'delete handler' });
+}
+
+exports.deleteAll = (req, res) => {
+    res.send({ message: 'deleteAll handler' });
+}
+
+exports.findAllPublished = (req, res) => {
+    res.send({ message: 'findAllPublished handler' });
+}
